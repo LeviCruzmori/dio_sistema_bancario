28f1fccb512603348ef09318c587889acd0eba8e
levi <dsciencemori@gmail.com>
2024-03-11 12:08:59 -0300

    codigo elaborado de forma condicional.

---
+++ doc/Redame.md
@@ -0,0 +1,5 @@
+# Desenvolvimento de um sistema bancario com refatoração de codigo.
+
+# srs/main.py
+Esta relacionado ao primeira versão do codigo, onde de forma basica e simples foi feito o desenvolvimento de algumas funções de forma condicional par aelaboração do codigo.
+
---
+++ src/main.py
@@ -0,0 +1,70 @@
+# criando o menu 
+menu = """
+
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+
+=> """
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+# criando condicional de operação
+while True:
+
+    opcao = input(menu)
+
+# criando acondicional para a função deposito
+    if opcao == "d":
+        valor = float(input("Informe o valor do depósito: "))
+
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R$ {valor:.2f}\n"
+
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+
+# criando condicional para a função saque.
+    elif opcao == "s":
+        valor = float(input("Informe o valor do saque: "))
+
+        excedeu_saldo = valor > saldo
+        excedeu_limite = valor > limite
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+        if excedeu_saldo:
+            print("Operação falhou! Você não tem saldo suficiente.")
+
+        elif excedeu_limite:
+            print("Operação falhou! O valor do saque excede o limite.")
+
+        elif excedeu_saques:
+            print("Operação falhou! Número máximo de saques excedido.")
+
+#criando a condicional para a função extrato.
+# \t deixa mais organizado,tipo tabbulado.
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque:\t\t R$ {valor:.2f}\n"
+            numero_saques += 1
+
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+
+    elif opcao == "e":
+        print("\n================ EXTRATO ================")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("==========================================")
+# condicioanl da função sair do menu.
+    elif opcao == "q":
+        break
+
+    else:
+        print("Operação inválida, por favor selecione novamente a operação desejada.")
